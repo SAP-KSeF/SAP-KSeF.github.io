4fb7e2fceebbaf65107100576d301be49c8f9f91
Jakub <jaqubowskyy@gmail.com>
2023-10-06 09:15:12 +0200

    Add new typing, extract to separate components

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,21 @@
 "use client";
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import TableContent from "@/components/TableContent";
+import TableWithContent from "@/components/TableWithContent";
 import { useSearchParams } from "next/navigation";
 import tableData from "../static/issues.json";
 
 type StateProps = "open" | "closed";
 type LabelProps = "new feature" | "bug";
 
-type Issue = (typeof tableData)[0];
+type FilterIssuesProps = {
+  state: StateProps;
+  label: LabelProps;
+};
 
 type TableDataProps = {
-  title: string;
-  data: typeof tableData;
+  title: "Planned Features" | "New Features" | "Bugs" | "Fixed Bugs";
+  content: React.ReactNode;
 };
 
 export default function Home() {
@@ -38,38 +35,20 @@ export default function Home() {
     );
   };
 
-  const renderTableContent = (data: Issue[]) => {
-    return data.map((issue, idx) => {
-      return (
-        <TableRow className="border-dotted border-2 hover:bg-muted/100" key={issue.number}>
-          <TableCell className="font-medium border-dotted border-2 w-[100px]">
-            {idx + 1}
-          </TableCell>
-          <TableCell className="break-words max-w-xs border-dotted border-2">
-            {issue.title}
-          </TableCell>
-        </TableRow>
-      );
+  const renderTableContent = ({ state, label }: FilterIssuesProps) => {
+    const tableDataContent = filterIssues({ state, label });
+
+    return tableDataContent.map((issue) => {
+      return <TableContent issue={issue} key={issue.number} />;
     });
   };
 
-  const renderTable = ({ title, data }: TableDataProps) => {
-    return (
-      <div className="max-w-5xl w-full p-11" key={title}>
-        <h1 className={`text-4xl font-bold mb-12 text-center`}>{title}</h1>
-        <Table>
-          <TableHeader >
-            <TableRow>
-              <TableHead className="w-[100px] border-2 border-dotted">
-                Number
-              </TableHead>
-              <TableHead className="border-2 border-dotted">Name</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>{renderTableContent(data)}</TableBody>
-        </Table>
-      </div>
-    );
+  const tableContent = tableData.map((issue) => {
+    return <TableContent issue={issue} key={issue.number} />;
+  });
+
+  const renderTableWithContent = ({ title, content }: TableDataProps) => {
+    return <TableWithContent title={title}>{content}</TableWithContent>;
   };
 
   return (
@@ -77,27 +56,33 @@ export default function Home() {
       <div className="flex flex-col gap-4 w-full justify-center items-center">
         {(searchParams.length === 0 ||
           searchParams.includes("planned-features")) &&
-          renderTable({
+          renderTableWithContent({
             title: "Planned Features",
-            data: filterIssues({ state: "open", label: "new feature" }),
+            content: renderTableContent({
+              state: "open",
+              label: "new feature",
+            }),
           })}
 
         {(searchParams.length === 0 || searchParams.includes("new-features")) &&
-          renderTable({
+          renderTableWithContent({
             title: "New Features",
-            data: filterIssues({ state: "closed", label: "new feature" }),
+            content: renderTableContent({
+              state: "closed",
+              label: "new feature",
+            }),
           })}
 
         {(searchParams.length === 0 || searchParams.includes("bugs")) &&
-          renderTable({
+          renderTableWithContent({
             title: "Bugs",
-            data: filterIssues({ state: "open", label: "bug" }),
+            content: renderTableContent({ state: "open", label: "bug" }),
           })}
 
         {(searchParams.length === 0 || searchParams.includes("fixed-bugs")) &&
-          renderTable({
+          renderTableWithContent({
             title: "Fixed Bugs",
-            data: filterIssues({ state: "closed", label: "bug" }),
+            content: renderTableContent({ state: "closed", label: "bug" }),
           })}
       </div>
     </main>
